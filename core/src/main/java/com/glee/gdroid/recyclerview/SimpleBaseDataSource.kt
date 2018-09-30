e7c997da060c6a46f7ee528db4b3671899dcf0a8
liji <liji@meixing.com>
2018-09-30 19:06:48 +0800

    update

--- core/src/main/java/com/glee/gdroid/recyclerview/SimpleBaseDataSource.kt
+++ core/src/main/java/com/glee/gdroid/recyclerview/SimpleBaseDataSource.kt
@@ -11,16 +11,20 @@ import android.arch.paging.PageKeyedDataSource
 
 
 abstract class SimpleBaseDataSource<V : BaseItemData> : PageKeyedDataSource<Int, V>() {
-    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, V>) {
-    }
+    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, V>) {}
 
-    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, V>) {
-    }
+
+    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, V>) = loadData(params, callback)
+
 
     override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Int, V>) {
+        loadBefore(LoadParams(0, params.requestedLoadSize), object : PageKeyedDataSource.LoadCallback<Int, V>() {
+            override fun onResult(data: MutableList<V>, adjacentPageKey: Int?) {
+                callback.onResult(data, -1, 1)
+            }
+        })
     }
 
-    abstract fun loadData(page: Int)
-
+    abstract fun loadData(params: LoadParams<Int>, callback: LoadCallback<Int, V>)
 
 }
